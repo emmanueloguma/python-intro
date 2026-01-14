223751be73f803dc03c27930c23d91bd9590c3c8
emmanueloguma <emmanueloguma60@gmail.com>
2026-01-14 10:55:33 +0100

    my second commit

--- add4.py
+++ add4.py
@@ -5,6 +5,10 @@ d=a+b+c
 print(d)
 if d<5000:
     print("yes it is less than 5000")
+    e=d*3
+    print("this is my final result",e)
 else:
     print("no it is greater than 5000")
+    e=d-5000
+    print("this is my final result",e)
         
